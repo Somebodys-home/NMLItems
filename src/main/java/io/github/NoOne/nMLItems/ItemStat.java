f02162b13ae17690cb8523fae8ba75e7a8b0d086
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-08-02 20:19:49 -0400

    added ItemStats and updated the ItemSystem

---
+++ src/main/java/io/github/NoOne/nMLItems/ItemStat.java
@@ -0,0 +1,105 @@
+package io.github.NoOne.nMLItems;
+
+import org.bukkit.ChatColor;
+
+import java.util.Map;
+
+public enum ItemStat {
+    // damage stats
+    PHYSICALDAMAGE,
+    FIREDAMAGE,
+    COLDDAMAGE,
+    EARTHDAMAGE,
+    LIGHTNINGDAMAGE,
+    AIRDAMAGE,
+    LIGHTDAMAGE,
+    DARKDAMAGE,
+    PUREDAMAGE,
+
+    // defense stats
+    EVASION,
+    DEFENSE,
+    OVERHEALTH,
+    BLOCK,
+    PHYSICALRESIST,
+    FIRERESIST,
+    COLDRESIST,
+    EARTHRESIST,
+    LIGHTNINGRESIST,
+    AIRRESIST,
+    LIGHTRESIST,
+    DARKRESIST;
+
+    public static String getStatString(ItemStat stat) {
+        String statString;
+
+        switch (stat) {
+            case PHYSICALDAMAGE -> statString = "Physical Damage";
+            case FIREDAMAGE -> statString = "Fire Damage";
+            case COLDDAMAGE -> statString = "Cold Damage";
+            case EARTHDAMAGE -> statString = "Earth Damage";
+            case LIGHTNINGDAMAGE -> statString = "Lightning Damage";
+            case AIRDAMAGE -> statString = "Air Damage";
+            case LIGHTDAMAGE -> statString = "Light Damage";
+            case DARKDAMAGE -> statString = "Dark Damage";
+            case PUREDAMAGE -> statString = "Pure Damage";
+            case EVASION -> statString = "Evasion";
+            case DEFENSE -> statString = "Defense";
+            case OVERHEALTH -> statString = "Overhealth";
+            case BLOCK -> statString = "Block";
+            case PHYSICALRESIST -> statString = "Physical Resist";
+            case FIRERESIST -> statString = "Fire Resist";
+            case COLDRESIST -> statString = "Cold Resist";
+            case EARTHRESIST -> statString = "Earth Resist";
+            case LIGHTNINGRESIST -> statString = "Lightning Resist";
+            case AIRRESIST -> statString = "Air Resist";
+            case LIGHTRESIST -> statString = "Light Resist";
+            case DARKRESIST -> statString = "Dark Resist";
+            default -> statString = "";
+        }
+
+        return statString;
+    }
+
+    public static ChatColor getStatColor(ItemStat stat) {
+        ChatColor color;
+
+        switch (stat) {
+            case EVASION, LIGHTDAMAGE, PUREDAMAGE, LIGHTRESIST, BLOCK -> color = ChatColor.WHITE;
+            case DEFENSE -> color = ChatColor.GREEN;
+            case OVERHEALTH -> color = ChatColor.DARK_BLUE;
+            case PHYSICALDAMAGE, PHYSICALRESIST -> color = ChatColor.DARK_RED;
+            case FIREDAMAGE, FIRERESIST -> color = ChatColor.RED;
+            case COLDDAMAGE, COLDRESIST -> color = ChatColor.AQUA;
+            case EARTHDAMAGE, EARTHRESIST -> color = ChatColor.DARK_GREEN;
+            case LIGHTNINGDAMAGE, LIGHTNINGRESIST -> color = ChatColor.YELLOW;
+            case AIRDAMAGE, AIRRESIST -> color = ChatColor.GRAY;
+            case DARKDAMAGE, DARKRESIST -> color = ChatColor.DARK_PURPLE;
+            default -> color = null;
+        }
+
+        return color;
+    }
+
+    public static String getStatEmoji(ItemStat stat) {
+        String statEmoji;
+
+        switch (stat) {
+            case PHYSICALDAMAGE, PHYSICALRESIST -> statEmoji = "⚔";
+            case FIREDAMAGE, FIRERESIST -> statEmoji = "\uD83D\uDD25";
+            case COLDDAMAGE, COLDRESIST -> statEmoji = "❄";
+            case EARTHDAMAGE, EARTHRESIST -> statEmoji = "\uD83E\uDEA8";
+            case LIGHTNINGDAMAGE, LIGHTNINGRESIST -> statEmoji = "\uD83D\uDDF2";
+            case AIRDAMAGE, AIRRESIST -> statEmoji = "☁";
+            case LIGHTDAMAGE, LIGHTRESIST -> statEmoji = "✦";
+            case DARKDAMAGE, DARKRESIST -> statEmoji = "\uD83C\uDF00";
+            case PUREDAMAGE -> statEmoji = "\uD83D\uDCA2";
+            case BLOCK, DEFENSE -> statEmoji = "\uD83D\uDEE1️";
+            case OVERHEALTH -> statEmoji = "\uD83D\uDC99";
+            case EVASION -> statEmoji = "\uD83E\uDEC6";
+            default -> statEmoji = "";
+        }
+
+        return statEmoji;
+    }
+}
--- src/main/java/io/github/NoOne/nMLItems/ItemSystem.java
+++ src/main/java/io/github/NoOne/nMLItems/ItemSystem.java
@@ -3,27 +3,144 @@ package io.github.NoOne.nMLItems;
 import io.github.NoOne.nMLPlayerStats.profileSystem.ProfileManager;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ItemSystem {
-    private NMLItems nmlItems;
-    private NamespacedKey originalNameKey;
-    private NamespacedKey levelKey;
+    private static NMLItems nmlItems;
+    private static NamespacedKey originalNameKey;
+    private static NamespacedKey levelKey;
 
     public ItemSystem(NMLItems nmlItems) {
-        this.nmlItems = nmlItems;
+        ItemSystem.nmlItems = nmlItems;
         originalNameKey = new NamespacedKey(nmlItems, "original_name");
         levelKey = new NamespacedKey(nmlItems, "level");
     }
 
-    public void updateUnusableItemName(ItemStack item, boolean usable) {
+    public static void setStat(ItemStack item, ItemStat stat, double amount) {
+        ItemMeta meta = item.getItemMeta();
+        assert meta != null;
+        PersistentDataContainer armorContainer = meta.getPersistentDataContainer();
+
+        armorContainer.set(makeKeyForStat(stat), PersistentDataType.DOUBLE, amount);
+        item.setItemMeta(meta);
+    }
+
+    public static boolean hasStat(ItemStack item, ItemStat stat) {
+        if (item == null) {
+            return false;
+        }
+
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer armorContainer = meta.getPersistentDataContainer();
+
+        return armorContainer.has(makeKeyForStat(stat), PersistentDataType.DOUBLE);
+    }
+
+    public static double getStatValue(ItemStack item, ItemStat stat) {
+        ItemMeta meta = item.getItemMeta();
+        assert meta != null;
+        PersistentDataContainer armorContainer = meta.getPersistentDataContainer();
+
+        if (hasStat(item, stat)) {
+            return armorContainer.get(makeKeyForStat(stat), PersistentDataType.DOUBLE);
+        }
+
+        return 0;
+    }
+
+    public static HashMap<ItemStat, Double> getAllStats(ItemStack item) {
+        HashMap<ItemStat, Double> stats = new HashMap<>();
+
+        for (ItemStat stat : ItemStat.values()) {
+            if (hasStat(item, stat)) {
+                stats.put(stat, getStatValue(item, stat));
+            }
+        }
+        return stats;
+    }
+
+    public static ItemType getItemTypeFromItemStack(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+
+        for (ItemType type : ItemType.values()) {
+            if (pdc.has(makeItemTypeKey(type), PersistentDataType.INTEGER)) return type;
+        }
+
+        return null;
+    }
+
+    public static ItemRarity getItemRarity(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+
+        for (ItemRarity rarity : ItemRarity.values()) {
+            if (pdc.has(makeItemRarityKey(rarity), PersistentDataType.INTEGER)) return rarity;
+        }
+
+        return null;
+    }
+
+    public static void updateLoreWithItemStats(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return;
+
+        List<String> originalLore = meta.hasLore() ? new ArrayList<>(meta.getLore()) : new ArrayList<>();
+        List<String> addedLore = new ArrayList<>();
+        HashMap<ItemStat, Double> itemStats = getAllStats(item);
+
+        itemStats.entrySet().stream()
+                .sorted((a, b) -> Double.compare(b.getValue(), a.getValue())) // Descending sort
+                .forEachOrdered(entry -> {
+                    double value = entry.getValue();
+                    int valueInt = (int) value;
+                    addedLore.add(ItemStat.getStatColor(entry.getKey()) + "+ " + valueInt + " " + ItemStat.getStatString(entry.getKey()));
+                });
+
+        originalLore.addAll(addedLore);
+        meta.setLore(originalLore);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        item.setItemMeta(meta);
+    }
+
+    public static String getOriginalItemName(ItemStack item) {
+        if (item == null || item.getType().isAir()) return null;
+
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return null;
+
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+        if (!pdc.has(originalNameKey, PersistentDataType.STRING)) return null;
+
+        return pdc.get(originalNameKey, PersistentDataType.STRING);
+    }
+
+    public static boolean isItemUsable(ItemStack item, Player player) {
+        if (item == null || !item.hasItemMeta()) return false;
+
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+        Integer itemLevel = pdc.get(levelKey, PersistentDataType.INTEGER);
+
+        if (itemLevel == null) return false;
+
+        int playerLevel = new ProfileManager(NMLItems.getNmlPlayerStats()).getPlayerProfile(player.getUniqueId()).getStats().getLevel();
+        return playerLevel >= itemLevel;
+    }
+
+    public static void updateUnusableItemName(ItemStack item, boolean usable) {
         if (item == null || !item.hasItemMeta()) return;
 
         ItemMeta meta = item.getItemMeta();
-        String originalName = this.getOriginalItemName(item);
+        String originalName = getOriginalItemName(item);
         String editedName;
 
         if (!usable) {
@@ -40,58 +157,15 @@ public class ItemSystem {
         }
     }
 
-    public boolean isItemUsable(ItemStack item, Player player) {
-        if (item == null || !item.hasItemMeta()) return false;
-
-        ItemMeta meta = item.getItemMeta();
-        PersistentDataContainer pdc = meta.getPersistentDataContainer();
-        Integer itemLevel = pdc.get(levelKey, PersistentDataType.INTEGER);
-
-        if (itemLevel == null) return false;
-
-        int playerLevel = new ProfileManager(NMLItems.getNmlPlayerStats()).getPlayerProfile(player.getUniqueId()).getStats().getLevel();
-        return playerLevel >= itemLevel;
+    private static NamespacedKey makeKeyForStat(ItemStat stat) {
+        return new NamespacedKey(nmlItems, ItemStat.getStatString(stat).replaceAll(" ", ""));
     }
 
-    public ItemType getItemTypeFromItemStack(ItemStack item) {
-        ItemMeta meta = item.getItemMeta();
-        PersistentDataContainer pdc = meta.getPersistentDataContainer();
-
-        for (ItemType type : ItemType.values()) {
-            if (pdc.has(makeItemTypeKey(type), PersistentDataType.INTEGER)) return type;
-        }
-
-        return null;
-    }
-
-    public ItemRarity getItemRarity(ItemStack item) {
-        ItemMeta meta = item.getItemMeta();
-        PersistentDataContainer pdc = meta.getPersistentDataContainer();
-
-        for (ItemRarity rarity : ItemRarity.values()) {
-            if (pdc.has(makeItemRarityKey(rarity), PersistentDataType.INTEGER)) return rarity;
-        }
-
-        return null;
-    }
-
-    public String getOriginalItemName(ItemStack item) {
-        if (item == null || item.getType().isAir()) return null;
-
-        ItemMeta meta = item.getItemMeta();
-        if (meta == null) return null;
-
-        PersistentDataContainer pdc = meta.getPersistentDataContainer();
-        if (!pdc.has(originalNameKey, PersistentDataType.STRING)) return null;
-
-        return pdc.get(originalNameKey, PersistentDataType.STRING);
-    }
-
-    public NamespacedKey makeItemTypeKey(ItemType type) {
+    public static NamespacedKey makeItemTypeKey(ItemType type) {
         return new NamespacedKey(nmlItems, ItemType.getItemTypeString(type));
     }
 
-    public NamespacedKey makeItemRarityKey(ItemRarity rarity) {
+    public static NamespacedKey makeItemRarityKey(ItemRarity rarity) {
         return new NamespacedKey(nmlItems, ItemRarity.getItemRarityString(rarity));
     }
 
