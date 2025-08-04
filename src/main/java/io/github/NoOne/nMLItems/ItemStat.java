0b1492734f0bd47624468e42fd63ebb65f0ff64a
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-08-03 21:08:37 -0400

    updated emojis x3

--- src/main/java/io/github/NoOne/nMLItems/ItemStat.java
+++ src/main/java/io/github/NoOne/nMLItems/ItemStat.java
@@ -94,7 +94,7 @@ public enum ItemStat {
             case LIGHTDAMAGE, LIGHTRESIST -> statEmoji = "✦";
             case DARKDAMAGE, DARKRESIST -> statEmoji = "\uD83C\uDF00";
             case PUREDAMAGE -> statEmoji = "\uD83D\uDCA2";
-            case BLOCK, DEFENSE -> statEmoji = "🛡️";
+            case BLOCK, DEFENSE -> statEmoji = "U+1F6E1";
             case OVERHEALTH -> statEmoji = "\uD83D\uDC99";
             case EVASION -> statEmoji = "\uD83D\uDCA8";
             default -> statEmoji = "";
